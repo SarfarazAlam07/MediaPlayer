3784f7fa7f01f53f7458a8151827265ab5864de3
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2026-04-06 17:40:03 +0530

    Added babel plugin and switched to Yarn

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin', // 🔥 Ye line Cloud server ko rasta dikhayegi
+    ],
   };
 };
