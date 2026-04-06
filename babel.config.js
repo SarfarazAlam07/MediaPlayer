6a2c5ae004ba592862c343a3e3a09217bb897417
Sarfaraz Alam <sarfaraz7277alam@gmail.com>
2026-04-06 18:08:31 +0530

    Nuked react-native-reanimated to fix gradle build

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // 🔥 Ye line Cloud server ko rasta dikhayegi
+      'react-native-reanimated/plugin', 
     ],
   };
 };
